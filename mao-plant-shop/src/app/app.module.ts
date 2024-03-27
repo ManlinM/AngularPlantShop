4ee8adaf1da197f992adcc651ce97cc802e27b94
Manlin Mao <marilynmao@live.cn>
2024-03-26 23:05:57 -0400

    initial commit for main container

--- mao-plant-shop/src/app/app.module.ts
+++ mao-plant-shop/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { MainContainerComponent } from './shared/main-container/main-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
